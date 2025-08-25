a54a269c1156f9d365e03d0be81ef08c840ca94f
Brian <brian.oneleguina@gmail.com>
2025-08-25 14:32:25 -0300

    Fix: ajustes en models, urls y base.html

--- proyecto_odontologico/urls.py
+++ proyecto_odontologico/urls.py
@@ -7,14 +7,8 @@ urlpatterns = [
     path("turnos/", include("turnos.urls")),
     path("pacientes/", include("pacientes.urls")),
     path("ficha_medica/", include("ficha_medica.urls")),
-<<<<<<< HEAD
-    path('caja/', include('caja.urls')), 
-    path('login/', include('login.urls')),
-   
-=======
     path("caja/", include("caja.urls")),
     path('admin/', admin.site.urls),
     path('caja/', include('caja.urls')), 
     
->>>>>>> 2c65912ca339491170783650d55c94db544aa425
 ]
