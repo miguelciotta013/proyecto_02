664d48e80d73aacef52237f87eb433ab95dc1a46
Miguel Ciotta <mamaniciotta@gmail.com>
2025-10-12 17:01:24 +0200

    back de panel_control y caja

--- caja/urls.py
+++ caja/urls.py
@@ -1,8 +1,30 @@
+# caja/urls.py
 from django.urls import path
-from . import views
-
-app_name = "caja"
+from .views import (
+    CajaListView,
+    CajaAperturaView,
+    CajaCierreView,
+    CajaDetailView,
+    CajaIngresoView,
+    CajaEgresoView,
+    MetodosCobroListView
+)
 
 urlpatterns = [
+    # Lista de cajas
+    path('', CajaListView.as_view(), name='caja-list'),
     
+    # Apertura y cierre
+    path('apertura/', CajaAperturaView.as_view(), name='caja-apertura'),
+    path('<int:id_caja>/cierre/', CajaCierreView.as_view(), name='caja-cierre'),
+    
+    # Detalle
+    path('<int:id_caja>/', CajaDetailView.as_view(), name='caja-detail'),
+    
+    # Ingresos y egresos
+    path('<int:id_caja>/ingresos/', CajaIngresoView.as_view(), name='caja-ingreso'),
+    path('<int:id_caja>/egresos/', CajaEgresoView.as_view(), name='caja-egreso'),
+    
+    # Métodos de cobro
+    path('metodos-cobro/', MetodosCobroListView.as_view(), name='metodos-cobro'),
 ]
